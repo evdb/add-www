c08694b47a6a080d03cf1544f4b1c0da0d0bf8aa
Edmund von der Burg <evdb@mysociety.org>
2012-08-21 13:27:06 +0200

    Implement free redirection page, switch to ejs

--- website/apps/redirector/app.js
+++ website/apps/redirector/app.js
@@ -3,19 +3,19 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+var express    = require('express'),
+    http       = require('http'),
+    path       = require('path'),
+    config     = require('config');
 
-var app = express();
+var app
+  = module.exports
+  = express();
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.set('view engine', 'ejs');
   app.use(express.bodyParser());
-  app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -24,6 +24,17 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('*', function (req, res) {
 
-module.exports = app;
+  var host = req.get('Host');
+
+  res.locals({
+    to_domain:         'www.' + host,
+    from_domain:       req.host, // strip port number
+    redirect_delay:    config.general.redirect_delay,
+    sales_site_name:   config.sales_app.name,
+    sales_site_domain: config.sales_app.host,
+  });
+  res.render('free.ejs');
+});
+
--- website/config/default.js
+++ website/config/default.js
@@ -1,8 +1,12 @@
 module.exports = {
+  general: {
+    redirect_delay: 5,
+  },
   server: {
     port: 3000,
   },
   sales_app: {
-    host: "www.127-0-0-1.org.uk",
+    name:   'add-www.com',
+    host:   'www.127-0-0-1.org.uk',
   },
 };
