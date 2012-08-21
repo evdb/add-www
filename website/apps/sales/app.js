0c18ee1875a104af71a33b271a7e78839978316e
Edmund von der Burg <evdb@mysociety.org>
2012-08-21 16:39:49 +0200

    Copy mockups over to sales app, make it respond to queries

--- website/apps/sales/app.js
+++ website/apps/sales/app.js
@@ -3,8 +3,25 @@
  * Module dependencies.
  */
 
-var express = require('express');
+var express    = require('express'),
+    path       = require('path'),
+    config     = require('config');
+  
 
-var app = express();
+var app = module.exports = express();
 
-module.exports = app;
+app.configure(function () {  
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
