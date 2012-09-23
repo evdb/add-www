7642ea9165a3333393b02bafa0937fdd13e65585
Edmund von der Burg <evdb@mysociety.org>
2012-09-23 11:07:15 +0100

    Move the compass config so that it cas be run from somewhere more sensible

--- website/apps/sales/config.rb
+++ website/apps/sales/config.rb
@@ -2,10 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path       = "/"
-sass_dir        = "/css"
-css_dir         = "/css"
-images_dir      = "images"
-javascripts_dir = "javascripts"
+
+sass_dir        = "public/scss"
+css_dir         = "public/css"
+images_dir      = "public/images"
+javascripts_dir = "public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
